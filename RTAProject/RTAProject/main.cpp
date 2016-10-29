72ed71e737ca2bda9bec241e128cdd4b6dc215dc
Ben Owens <browens1995@gmail.com>
2016-10-29 03:19:07 -0400

    DirectX essentials (Swapchain, RTV, Viewports, etc) Initialized DirectMath & Matrices added DeltaTime calculation added

--- RTAProject/RTAProject/main.cpp
+++ RTAProject/RTAProject/main.cpp
@@ -3,8 +3,11 @@
 #include <iostream>
 #include <d3d11.h>
 #pragma comment(lib,"d3d11.lib")
+#include <DirectXMath.h>
 
 #include <cmath>
+#include "xTime.h"
+using namespace DirectX;
 using namespace std;
 
 #define BUFFER_WIDTH	800
@@ -15,6 +18,12 @@ HWND hWnd;
 //Windows App Class
 class APP
 {
+	struct SCENE_BUFFER
+	{
+		XMMATRIX m4x4_view;
+		XMMATRIX m4x4_projection;
+	};
+
 	HINSTANCE	h_application;
 	WNDPROC		appWndProc;
 	HWND		h_window;
@@ -26,6 +35,8 @@ class APP
 	IDXGISwapChain* p_swapchain;
 	D3D11_VIEWPORT viewport;
 
+	SCENE_BUFFER scene_matrix;
+
 	//Buffers
 	ID3D11Buffer* p_sceneBuffer;
 
@@ -36,6 +47,13 @@ class APP
 	
 	ID3D11RasterizerState* p_rsSolid;
 	ID3D11RasterizerState* p_rsWireframe;
+
+	XMMATRIX m4x4_camera;
+	XMMATRIX m4x4_camTranslate;
+
+	XTime xTime;
+
+	int moveSpeed;
 
 public:
 	APP(HINSTANCE hinst, WNDPROC proc);
@@ -73,6 +91,16 @@ APP::APP(HINSTANCE hinst, WNDPROC proc)
 
 	hWnd = h_window;
 //////////////////
+
+
+	//Matrices
+	scene_matrix.m4x4_view = XMMatrixInverse(NULL, XMMatrixIdentity());
+	scene_matrix.m4x4_view.r[3].m128_f32[2] = -2.0f;
+	scene_matrix.m4x4_projection = XMMatrixPerspectiveFovLH(65, 1, 0.1, 500);
+
+	m4x4_camera = XMMatrixIdentity();
+	m4x4_camera.r[3].m128_f32[2] = -2.0f;
+	m4x4_camTranslate = XMMatrixIdentity();
 
 	//Swapchain Descriptor
 	DXGI_SWAP_CHAIN_DESC sc_Descrip;
@@ -125,6 +153,7 @@ APP::APP(HINSTANCE hinst, WNDPROC proc)
 	ZeroMemory(&bBuffer, sizeof(bBuffer));
 	p_swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&bBuffer);
 	p_device->CreateRenderTargetView(bBuffer, NULL, &p_RTV);
+	bBuffer->Release();
 
 	//Viewport
 	ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
@@ -135,6 +164,63 @@ APP::APP(HINSTANCE hinst, WNDPROC proc)
 	viewport.MaxDepth = 1.0f;
 
 	//Scene Buffer
+	D3D11_BUFFER_DESC sb_Descrip;
+	ZeroMemory(&sb_Descrip, sizeof(sb_Descrip));
+	sb_Descrip.ByteWidth = sizeof(SCENE_BUFFER);
+	sb_Descrip.Usage = D3D11_USAGE_DYNAMIC;
+	sb_Descrip.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
+	sb_Descrip.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+	sb_Descrip.MiscFlags = 0;
+	sb_Descrip.StructureByteStride = 0;
+
+	p_device->CreateBuffer(&sb_Descrip, NULL, &p_sceneBuffer);
+
+	//Depth Stencil
+	D3D11_TEXTURE2D_DESC ds_texDescrip;
+	ZeroMemory(&ds_texDescrip, sizeof(ds_texDescrip));
+	ds_texDescrip.Width = BUFFER_WIDTH;
+	ds_texDescrip.Height = BUFFER_HEIGHT;
+	ds_texDescrip.MipLevels = 1;
+	ds_texDescrip.ArraySize = 1;
+	ds_texDescrip.Format = DXGI_FORMAT_D32_FLOAT;
+	ds_texDescrip.SampleDesc.Count = 1;
+	ds_texDescrip.SampleDesc.Quality = 0;
+	ds_texDescrip.Usage = D3D11_USAGE_DEFAULT;
+	ds_texDescrip.BindFlags = D3D11_BIND_DEPTH_STENCIL;
+	ds_texDescrip.CPUAccessFlags = 0;
+
+	p_device->CreateTexture2D(&ds_texDescrip, NULL, &p_depthStencil);
+
+	D3D11_DEPTH_STENCIL_DESC ds_Descrip;
+	ZeroMemory(&ds_Descrip, sizeof(ds_Descrip));
+
+	ds_Descrip.DepthEnable = true;
+	ds_Descrip.DepthWriteMask = D3D11_DEPTH_WRITE_MASK_ALL;
+	ds_Descrip.DepthFunc = D3D11_COMPARISON_LESS;
+
+	ds_Descrip.StencilEnable = true;
+	ds_Descrip.StencilReadMask = 0xff;
+	ds_Descrip.StencilWriteMask = 0xff;
+
+	ds_Descrip.FrontFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+	ds_Descrip.FrontFace.StencilDepthFailOp = D3D11_STENCIL_OP_INCR;
+	ds_Descrip.FrontFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+	ds_Descrip.FrontFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+
+	ds_Descrip.BackFace.StencilFailOp = D3D11_STENCIL_OP_KEEP;
+	ds_Descrip.BackFace.StencilDepthFailOp = D3D11_STENCIL_OP_DECR;
+	ds_Descrip.BackFace.StencilPassOp = D3D11_STENCIL_OP_KEEP;
+	ds_Descrip.BackFace.StencilFunc = D3D11_COMPARISON_ALWAYS;
+
+	p_device->CreateDepthStencilState(&ds_Descrip, &p_dsState);
+
+	D3D11_DEPTH_STENCIL_VIEW_DESC ds_viewDescrip;
+	ZeroMemory(&ds_viewDescrip, sizeof(ds_viewDescrip));
+	ds_viewDescrip.Format = DXGI_FORMAT_D32_FLOAT;
+	ds_viewDescrip.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
+	ds_viewDescrip.Texture2D.MipSlice = 0;
+
+	p_device->CreateDepthStencilView(p_depthStencil, &ds_viewDescrip, &p_dsView);
 
 
 	p_application = this;
@@ -145,14 +231,36 @@ bool APP::Run()
 	//Clear To Back Color
 	const float backColor[4] = { 0, 0, 0.4, 1 };
 	p_context->ClearRenderTargetView(p_RTV, backColor);
-	//p_context->ClearDepthStencilView(p_dsView, D3D11_CLEAR_DEPTH, 1.0f, 0);
+	p_context->ClearDepthStencilView(p_dsView, D3D11_CLEAR_DEPTH, 1.0f, 0);
 
 	//Set Depth Stencil & Viewport
-	//p_context->OMSetDepthStencilState(p_dsState, 1);
-	//p_context->OMSetRenderTargets(1, &p_RTV, p_dsView);
+	p_context->OMSetDepthStencilState(p_dsState, 1);
+	p_context->OMSetRenderTargets(1, &p_RTV, p_dsView);
 
 	p_context->RSSetViewports(1, &viewport);
 	p_context->RSSetState(p_rsSolid);
+
+	if (GetAsyncKeyState(VK_SHIFT))
+		moveSpeed = 15;
+	else
+		moveSpeed = 7;
+
+	if (GetAsyncKeyState('W'))
+	{
+		m4x4_camTranslate.r[3].m128_f32[2] = moveSpeed * xTime.Delta();
+		m4x4_camera = XMMatrixMultiply(m4x4_camTranslate, m4x4_camera);
+	}
+
+	if (GetAsyncKeyState('S'))
+	{
+		m4x4_camTranslate.r[3].m128_f32[2] = -moveSpeed * xTime.Delta();
+		m4x4_camera = XMMatrixMultiply(m4x4_camTranslate, m4x4_camera);
+	}
+
+	D3D11_MAPPED_SUBRESOURCE map_res;
+	p_context->Map(p_sceneBuffer, NULL, D3D11_MAP_WRITE_DISCARD, 0, &map_res);
+	memcpy(map_res.pData, &scene_matrix, sizeof(SCENE_BUFFER));
+	p_context->Unmap(p_sceneBuffer, 0);
 
 	p_swapchain->Present(0, 0);
 	return true;
@@ -161,6 +269,19 @@ bool APP::Run()
 
 bool APP::ShutDown()
 {
+	p_swapchain->Release();
+	p_RTV->Release();
+	p_device->Release();
+	p_context->Release();
+	
+	p_sceneBuffer->Release();
+	
+	p_depthStencil->Release();
+	p_dsState->Release();
+	p_dsView->Release();
+	p_rsSolid->Release();
+	p_rsWireframe->Release();
+
 	UnregisterClass(L"DirectXApplication", h_application);
 	return true;
 }
