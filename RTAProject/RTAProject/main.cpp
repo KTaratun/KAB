d234fe50818a3a69c5dc09eb163ef6d0317b0919
Ben <br_owens1116@comcast.net>
2016-10-28 20:04:49 -0400

    DirectX Library Added Clears Screen to Default Color

--- RTAProject/RTAProject/main.cpp
+++ RTAProject/RTAProject/main.cpp
@@ -1,7 +1,10 @@
 #include <Windows.h>
 #include <ctime>
 #include <iostream>
+#include <d3d11.h>
+#pragma comment(lib,"d3d11.lib")
 
+#include <cmath>
 using namespace std;
 
 #define BUFFER_WIDTH	800
@@ -16,16 +19,35 @@ class APP
 	WNDPROC		appWndProc;
 	HWND		h_window;
 
+	//Device, Swapchain, Viewport,etc
+	ID3D11Device* p_device;
+	ID3D11DeviceContext* p_context;
+	ID3D11RenderTargetView* p_RTV;
+	IDXGISwapChain* p_swapchain;
+	D3D11_VIEWPORT viewport;
+
+	//Buffers
+	ID3D11Buffer* p_sceneBuffer;
+
+	//Depth Stencil
+	ID3D11Texture2D* p_depthStencil = NULL;
+	ID3D11DepthStencilState* p_dsState;
+	ID3D11DepthStencilView* p_dsView;
+	
+	ID3D11RasterizerState* p_rsSolid;
+	ID3D11RasterizerState* p_rsWireframe;
+
 public:
 	APP(HINSTANCE hinst, WNDPROC proc);
 	bool Run();
 	bool ShutDown();
-
+	
 };
 APP* p_application;
 
 APP::APP(HINSTANCE hinst, WNDPROC proc)
 {
+/////////// WINDOWS CODE
 	h_application = hinst;
 	appWndProc = proc;
 
@@ -50,13 +72,89 @@ APP::APP(HINSTANCE hinst, WNDPROC proc)
 	ShowWindow(h_window, SW_SHOW);
 
 	hWnd = h_window;
+//////////////////
+
+	//Swapchain Descriptor
+	DXGI_SWAP_CHAIN_DESC sc_Descrip;
+	ZeroMemory(&sc_Descrip, sizeof(sc_Descrip));
+
+	sc_Descrip.BufferCount = 1;
+	sc_Descrip.BufferDesc.Width = BUFFER_WIDTH;
+	sc_Descrip.BufferDesc.Height = BUFFER_HEIGHT;
+	sc_Descrip.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
+	sc_Descrip.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
+	sc_Descrip.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sc_Descrip.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sc_Descrip.OutputWindow = h_window;
+	sc_Descrip.SampleDesc.Count = 1;
+	sc_Descrip.SampleDesc.Quality = 0;
+	sc_Descrip.Windowed = true;
+	sc_Descrip.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH;
+	sc_Descrip.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+
+	UINT flag;
+
+	if (_DEBUG)
+		flag = D3D11_CREATE_DEVICE_DEBUG;
+	else
+		flag = 1;
+
+	D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, flag, NULL, NULL, D3D11_SDK_VERSION, &sc_Descrip, &p_swapchain, &p_device, NULL, &p_context);
+
+	//Create Raster States
+	D3D11_RASTERIZER_DESC rs_solidDescrip;
+	rs_solidDescrip.AntialiasedLineEnable = false;
+	rs_solidDescrip.CullMode = D3D11_CULL_BACK;
+	rs_solidDescrip.DepthBias = 0;
+	rs_solidDescrip.DepthBiasClamp = 0.0f;
+	rs_solidDescrip.DepthClipEnable = true;
+	rs_solidDescrip.FillMode = D3D11_FILL_SOLID;
+	rs_solidDescrip.FrontCounterClockwise = false;
+	rs_solidDescrip.MultisampleEnable = false;
+	rs_solidDescrip.ScissorEnable = false;
+	rs_solidDescrip.SlopeScaledDepthBias = 0.0f;
+
+	D3D11_RASTERIZER_DESC rs_wireframeDescrip = rs_solidDescrip;
+	rs_wireframeDescrip.FillMode = D3D11_FILL_WIREFRAME;
+
+	p_device->CreateRasterizerState(&rs_solidDescrip, &p_rsSolid);
+	p_device->CreateRasterizerState(&rs_wireframeDescrip, &p_rsWireframe);
+
+	//Backbuffer & RTV
+	ID3D11Texture2D* bBuffer;
+	ZeroMemory(&bBuffer, sizeof(bBuffer));
+	p_swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), (LPVOID*)&bBuffer);
+	p_device->CreateRenderTargetView(bBuffer, NULL, &p_RTV);
+
+	//Viewport
+	ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = sc_Descrip.BufferDesc.Width;
+	viewport.Height = sc_Descrip.BufferDesc.Height;
+	viewport.MaxDepth = 1.0f;
+
+	//Scene Buffer
+
 
 	p_application = this;
 }
 
 bool APP::Run()
 {
-	
+	//Clear To Back Color
+	const float backColor[4] = { 0, 0, 0.4, 1 };
+	p_context->ClearRenderTargetView(p_RTV, backColor);
+	//p_context->ClearDepthStencilView(p_dsView, D3D11_CLEAR_DEPTH, 1.0f, 0);
+
+	//Set Depth Stencil & Viewport
+	//p_context->OMSetDepthStencilState(p_dsState, 1);
+	//p_context->OMSetRenderTargets(1, &p_RTV, p_dsView);
+
+	p_context->RSSetViewports(1, &viewport);
+	p_context->RSSetState(p_rsSolid);
+
+	p_swapchain->Present(0, 0);
 	return true;
 }
 
